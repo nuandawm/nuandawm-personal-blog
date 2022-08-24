6c704ac01e177587ee32a5c2876fbb4b0dfd8261
nuandawm <derossi.giuliano@gmail.com>
2022-08-24 19:52:05 +0200

    Update personal info in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,27 @@ module.exports = {
             sizes: "16x16",
             type: "image/svg",
           }
-        ]
+        ],
+
+        website: {
+          twitterName: "NuandaWM",
+          url: "https://nuandawm.xyz/",
+          copyright: "© Copyright 2022 | Giuliano De Rossi",
+          logoUrl: "/logos/G2_logo.svg",
+        },
+
+        user: {
+          id: "NuandaWM",
+          firstName: "Nuanda",
+          lastName: "Fox",
+          twitterName: "NuandaWM",
+          linkedIn: "giuliano-de-rossi-a8966127",
+          github: "nuandawm",
+          email: "suxgiu@krasnij-njesa.com",
+          location: "User Location",
+          about: "A full-stack web developer looking for a challenge!",
+          avatar: "https://i.pravatar.cc/300",
+        },
       },
     },
   ],
