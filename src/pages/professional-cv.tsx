7e0fef1093489ddb2afdc959220c6428dcac1429
nuandawm <derossi.giuliano@gmail.com>
2022-08-29 02:06:21 +0200

    Professional CV - WIP Add minor style fix

--- src/pages/professional-cv.tsx
+++ src/pages/professional-cv.tsx
@@ -112,7 +112,7 @@ const ProfessionalCVPage = ({data}: ProfessionalCVPageProps): JSX.Element => {
               {personalInfoData.map((info, i) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <li key={i}>
-                  <b>{info.label}:</b> {info.value}
+                  <p><b>{info.label}:</b> {info.value}</p>
                 </li>
               ))}
             </CleanList>
