f7dbca1c9290dfc08f2ec18709152071a5b94fac
nuandawm <derossi.giuliano@gmail.com>
2022-09-02 15:00:47 +0200

    Quick fix to remove gaps between timelines

--- src/components/TimeProgressionSVG/index.tsx
+++ src/components/TimeProgressionSVG/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
+
+const HEIGHT_QUICK_FIX = 15;
 
 const SVGWrapper = styled.svg`
   width: 20px;
@@ -27,7 +29,21 @@ type TimeProgressionSVGProps = {
   isLast?: boolean;
 }
 
-const TimeProgressionSVG = ({timelineThickness, timelineHeight, isLast, isFirst}: TimeProgressionSVGProps) => {
+const TimeProgressionSVG = ({timelineThickness, timelineHeight, isFirst, isLast}: TimeProgressionSVGProps) => {
+  // This is not right, but I didn't know how to do this without a ResizeObserver
+  const calculateTopYCorrection = useCallback(
+    (y: number) => isFirst ? y : y - HEIGHT_QUICK_FIX,
+    [isFirst]
+  )
+  const calculateTopHeightCorrection = useCallback(
+    (height: number) => isFirst ? height : height + HEIGHT_QUICK_FIX,
+    [isFirst]
+  )
+  const calculateBottomHeightCorrection = useCallback(
+    (height: number) => isLast ? height : height + HEIGHT_QUICK_FIX,
+    [isLast]
+  )
+
   return <SVGWrapper viewBox={`0 0 20 ${timelineHeight || 10}`} xmlns="http://www.w3.org/2000/svg">
     <defs>
       <linearGradient id="last-gradient" x1="0" x2="0" y1="0" y2="1">
@@ -41,11 +57,11 @@ const TimeProgressionSVG = ({timelineThickness, timelineHeight, isLast, isFirst}
     </defs>
     {timelineHeight &&
       <>
-        <rect width={timelineThickness} x={5} y={0}
-              height={50}
+        <rect width={timelineThickness} x={5} y={calculateTopYCorrection(0)}
+              height={calculateTopHeightCorrection(50)}
               fill={isFirst ? 'url(#first-gradient)' : 'var(--color-grey-600)'}/>
         <rect width={timelineThickness} x={5} y={50}
-              height={timelineHeight - 50}
+              height={calculateBottomHeightCorrection(timelineHeight - 50)}
               fill={isLast ? 'url(#last-gradient)' : 'var(--color-grey-600)'}/>
       </>}
     <circle cx={10} cy={50} r={10}/>
